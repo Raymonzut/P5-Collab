2fe4781a82aa2e68086529e4674255160a1fc6bd
Raymonzut <40148684+Raymonzut@users.noreply.github.com>
2019-03-21 17:58:08 +0100

    Basic circle rotation

--- js/sketch.js
+++ js/sketch.js
@@ -1,10 +1,44 @@
+let innerR = 100;
+let outerR = 60;
+
+let precision = 0.5;
+let t = 0;
+
 const bgColor = 40;
 
 function setup() {
   createCanvas(500, 500);
   background(bgColor);
+  frameRate(60);
+}
+
+function drawInner() {
+  noFill();
+  stroke(255);
+  circle(width / 2, height / 2, innerR);
+}
+
+function drawOuter() {
+  let x = (width / 2) + (innerR + outerR) * cos(t);
+  let y = (height / 2) + (innerR + outerR) * sin(t);
+
+  noFill();
+  stroke(255);
+  circle(x, y, outerR);
+}
+
+function step() {
+  let fps = frameRate();
+  if (fps != 0) {
+    t += precision / fps;
+  }
 }
 
 function draw() {
   background(bgColor);
+
+  drawInner();
+  drawOuter();
+
+  step();
 }
